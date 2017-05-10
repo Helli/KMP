78a40ded8635f7a374d82662d1eafbf3bdc85c45
Helli <efbfi@web.de>
2017-05-10 15:24:03 +0200

    minor clarifications

--- KMP.thy
+++ KMP.thy
@@ -3,9 +3,7 @@
 theory KMP
   imports "$AFP/Refine_Imperative_HOL/IICF/IICF"
 begin
-  
-  term "SPEC (\<lambda>x::nat. x \<in> {4,7,9})"
-  
+
 section\<open>Definition "substring"\<close>
   definition "is_substring_at' t s i \<equiv> take (length s) (drop i t) = s"  
   
@@ -13,6 +11,7 @@ section\<open>Definition "substring"\<close>
   value "is_substring_at' [5] [] 5"
   value "is_substring_at' [5] [5] 5"
   value "is_substring_at' [] [] 3"
+  text\<open>Not very intuitive...\<close>
   
   text\<open>For the moment, we use this instead:\<close>
   fun is_substring_at where
@@ -20,22 +19,34 @@ section\<open>Definition "substring"\<close>
     "is_substring_at (t#ts) ss (Suc i) \<longleftrightarrow> is_substring_at ts ss i" |
     "is_substring_at t [] 0 \<longleftrightarrow> True" |
     "is_substring_at [] _ _ \<longleftrightarrow> False"
-  
+
   value "is_substring_at [5] [] 5"
   value "is_substring_at [5] [5] 5"
   value "is_substring_at [] [] 3"
   text\<open>Better, I suppose?\<close>
-        
+  
+  text\<open>For all relevant cases, both definitions agree:\<close>
   lemma "i \<le> length t \<Longrightarrow> is_substring_at t s i \<longleftrightarrow> is_substring_at' t s i"
     unfolding is_substring_at'_def
     by (induction t s i rule: is_substring_at.induct) auto
       
+  text\<open>However, the new definition has some reasonable properties:\<close>
+  lemma substring_length_s: "is_substring_at t s i \<Longrightarrow> length s \<le> length t"
+    apply (induction t s i rule: is_substring_at.induct)
+    apply simp_all
+    done
+  
+  lemma substring_i: "is_substring_at t s i \<Longrightarrow> i \<le> length t - length s"
+    apply (induction t s i rule: is_substring_at.induct)
+    apply (auto simp add: Suc_diff_le substring_length_s)
+    done
+
   (*Todo: third alternative: inductive is_substring_at*)
 
 section\<open>Naive algorithm\<close>
     
 subsection\<open>Invariants\<close>
-  definition "I_out_na t s \<equiv> \<lambda>(i,j(*_?*),found).
+  definition "I_out_na t s \<equiv> \<lambda>(i,j,found).
     \<not>found \<and> j = 0 \<and> (\<forall>i' < i. \<not>is_substring_at t s i')
     \<or> found \<and> is_substring_at t s i"
   definition "I_in_na t s iout (*KMP should need jout, too*) \<equiv> \<lambda>(j,found).
@@ -46,6 +57,7 @@ subsection\<open>Algorithm\<close>
   definition "na t s \<equiv> do {
     let i=0;
     let j=0;
+    let found=False;
     (_,_,found) \<leftarrow> WHILEI (I_out_na t s) (\<lambda>(i,j,found). i\<le>length t - length s \<and> \<not>found) (\<lambda>(i,j,found). do {
       (j,found) \<leftarrow> WHILEI (I_in_na t s i) (\<lambda>(j,found). t!(i+j) = s!j \<and> \<not>found) (\<lambda>(j,found). do {
         let j=j+1;
@@ -56,7 +68,7 @@ subsection\<open>Algorithm\<close>
         let j = 0;
         RETURN (i,j,False)
       } else RETURN (i,j,True)
-    }) (i,j,False);
+    }) (i,j,found);
 
     RETURN found
   }"
@@ -67,8 +79,7 @@ lemma substring_step:
   apply auto
   using is_substring_at.elims(3) by fastforce
 
-lemma empty_substring: "i \<le> length t
-         \<Longrightarrow> is_substring_at t [] i"
+lemma empty_is_substring: "i \<le> length t \<Longrightarrow> is_substring_at t [] i"
   apply (induction t arbitrary: i)
   apply auto
   using is_substring_at.elims(3) by force
@@ -77,9 +88,9 @@ lemma all_positions_substring:
   "\<lbrakk>length s \<le> length t; i \<le> length t - length s;
     \<forall>j'<length s. t ! (i + j') = s ! j'\<rbrakk>
        \<Longrightarrow> is_substring_at t s i"
-  proof (induction s arbitrary: t i rule: rev_induct)
+  proof (induction s rule: rev_induct)
     case Nil
-    then show ?case by (simp add: empty_substring)
+    then show ?case by (simp add: empty_is_substring)
   next
     case (snoc x xs)
     from `length (xs @ [x]) \<le> length t` have "length xs \<le> length t" by simp
@@ -101,25 +112,17 @@ lemma substring_all_positions:
   by (induction t s i rule: is_substring_at.induct)
     (auto simp: nth_Cons')
 
-lemma substring_smaller: "is_substring_at t s i \<Longrightarrow> length s \<le> length t"
-  by (induction t s i rule: is_substring_at.induct) simp_all
-
-lemma i_bounded: "is_substring_at t s i \<Longrightarrow> i \<le> length t - length s"
-  apply (induction t s i rule: is_substring_at.induct)
-  apply (auto simp add: Suc_diff_le substring_smaller)
-  done
-
 lemma "\<lbrakk>s \<noteq> []; t \<noteq> []; length s \<le> length t\<rbrakk>
   \<Longrightarrow> na t s \<le> SPEC (\<lambda>r. r \<longleftrightarrow> (\<exists>i. is_substring_at t s i))"
     unfolding na_def I_out_na_def I_in_na_def
-    (*are these safe?*)apply refine_vcg apply vc_solve
+    apply refine_vcg apply vc_solve
     apply (metis all_positions_substring less_SucE)
     using less_Suc_eq apply blast
     apply (metis less_SucE substring_all_positions)
-    by (meson i_bounded leI le_less_trans)
-
+    by (meson leI le_less_trans substring_i)
+    
 text\<open>These preconditions cannot be removed:
-  If "s = []" or "t = []", the inner while-condition will access out-of-bound memory. The same can happen if "length t < length s" (I guess it could be narrowed down to something like 't not a prefix of s', but that's a bit pointless).\<close>
+  If @{term \<open>s = []\<close>} or  @{term \<open>t = []\<close>}, the inner while-condition will access out-of-bound memory. The same can happen if @{term \<open>length t < length s\<close>} (I guess this one could be narrowed down to something like "if t is a proper prefix of s", but that's a bit pointless).\<close>
 (*ToDo: WHILET statt WHILE*)
       
 section\<open>Knuth–Morris–Pratt algorithm\<close>
@@ -167,6 +170,8 @@ subsection\<open>Algorithm\<close>
       
 section\<open>Notes and Tests\<close>
 
+  term "SPEC (\<lambda>x::nat. x \<in> {4,7,9})"
+  
   term "\<lambda>t s. SPEC (\<lambda>None \<Rightarrow> \<nexists>i. is_substring_at t s i | Some i \<Rightarrow> is_substring_at t s i \<and> (\<forall>j. is_substring_at t s j \<longrightarrow> i\<le>j))"
   
   
@@ -201,3 +206,9 @@ section\<open>Notes and Tests\<close>
     apply auto  
     done
     
+  value[simp] "\<not>is_substring_at [5::nat] [] 5"
+  value[nbe] "\<not>is_substring_at [5::nat] [] 5"
+  value[code] "\<not>is_substring_at [5::nat] [] 5"
+  lemma "\<not>is_substring_at [5::nat] [] 5"
+    by (simp add: eval_nat_numeral(3) numeral_code(2))
+  text\<open>What is going on?\<close>
